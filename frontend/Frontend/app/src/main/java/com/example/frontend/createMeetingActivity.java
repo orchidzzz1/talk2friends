499c40e56de5b83572e8e3ed97f0ce851beddb87
Easton Zhang <eastonzhang01@gmail.com>
2023-11-05 01:24:10 -0800

    update verification page and add button functionality

--- frontend/Frontend/app/src/main/java/com/example/frontend/createMeetingActivity.java
+++ frontend/Frontend/app/src/main/java/com/example/frontend/createMeetingActivity.java
@@ -2,13 +2,32 @@ package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class createMeetingActivity extends AppCompatActivity {
+
+    private Button createMeetingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meeting);
+
+        // initialize create meeting button
+        createMeetingButton = (Button) findViewById(R.id.createNewMeetingButton);
+        createMeetingButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMeetingsPage();
+            }
+        });
+    }
+
+    public void openMeetingsPage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
